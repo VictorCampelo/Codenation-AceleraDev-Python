89a366d4b1bab49b9a56461ee6494a498b697d64
Victor Campelo <victor_campelo@outlook.com>
2020-07-08 20:24:06 -0300

    export model created on Django app api to codenation model file to on-line test and submit solved problem

---
+++ WEEK 5/python-9/api/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    last_login = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(max_length = 254)
+    password = models.CharField(max_length=50)
+    objects = models.Manager()
+
+
+class Agent(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    status = models.BooleanField()
+    env = models.CharField(max_length=20)
+    version = models.CharField(max_length=5)
+    address = models.GenericIPAddressField(protocol="IPV4", default="0.0.0.0")
+    objects = models.Manager()
+
+def level_validate(value):
+    if value in ['CRITICAL', 'DEBUG', 'ERROR', 'WARNING', 'INFO']:
+        return value
+    else:
+        raise ValidationError("Invalid value")
+
+class Event(models.Model):
+    id = models.AutoField(primary_key=True)
+    level = models.CharField(max_length=20, validators=[level_validate])
+    data = models.TextField()
+    arquivado = models.BooleanField()
+    date = models.DateField(auto_now=True)
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    objects = models.Manager()
+
+class Group(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    objects = models.Manager()
+    
+class GroupUser(models.Model):
+    id = models.AutoField(primary_key=True)
+    group = models.OneToOneField(Group, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    objects = models.Manager()
